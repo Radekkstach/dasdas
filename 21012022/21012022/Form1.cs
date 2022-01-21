611b04888c1f95ae57521eaedf171187c759ee8a
Radekkstach <97514874+Radekkstach@users.noreply.github.com>
2022-01-21 13:16:17 +0100

    Add files via upload

---
+++ 21012022/21012022/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21012022
+{
+    public partial class Form1 : Form
+    {
+        int kliknuti;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double pocet = 0;
+            double soucet = 0;
+            for(int i = 0; i< textBox1.Lines.Length ;i++)
+            {
+                pocet++;
+                soucet += int.Parse(textBox1.Lines[i]);
+            }
+            MessageBox.Show(string.Format("Vysledny prumer je {0}!", soucet / pocet));
+        }
+        
+    }
+}
